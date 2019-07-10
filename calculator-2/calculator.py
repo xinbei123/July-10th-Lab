1f1ae07c9153ec372df4b0a6c69999ac5b40c252
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-10 16:24:28 -0700

    Defined function, fixed print

--- calculator-2/calculator.py
+++ calculator-2/calculator.py
@@ -7,50 +7,48 @@ calculator program yourself in this file.
 from arithmetic import * 
 
 
-# Your code goes here
-while True:
+def calculator_2():
 
-    input_string = input("enter something ")
-    tokens = input_string.split(" ")
+    while True:
 
-    if len(tokens) < 3 and len(tokens) > 1:
-        num1 = int(tokens[1])
-    elif len(tokens) == 3:
-        num1 = int(tokens[1])
-        num2 = int(tokens[2])
+        input_string = input("enter something ")
+        tokens = input_string.split(" ")
+        answer = None
+
+        if len(tokens) < 3 and len(tokens) > 1:
+            num1 = int(tokens[1])
+        elif len(tokens) == 3:
+            num1 = int(tokens[1])
+            num2 = int(tokens[2])
 
 
-    if input_string == "q":
-        break
+        if input_string == "q":
+            break
 
-    elif tokens[0] == "-":
-        answer = subtract(num1, num2)
+        elif tokens[0] == "-":
+            answer = subtract(num1, num2)
+
+        elif tokens[0] == "+":
+            answer = add(num1, num2)
+
+        elif tokens[0] == "*":
+            answer = multiply(num1, num2)
+
+        elif tokens[0] == "/":
+            answer = divide(num1, num2)
+
+        elif tokens[0] == "square":
+            answer = square(num1)
+
+        elif tokens[0] == "cube":
+            answer = cube(num1)
+
+        elif tokens[0] == "power":
+            answer = power(num1, num2)
+
+        elif tokens[0] == "mod":
+            answer = mod(num1, num2)
+
         print(answer)
 
-    elif tokens[0] == "+":
-        answer = add(num1, num2)
-        print(answer)
-
-    elif tokens[0] == "*":
-        answer = multiply(num1, num2)
-        print(answer)
-
-    elif tokens[0] == "/":
-        answer = divide(num1, num2)
-        print(answer)
-
-    elif tokens[0] == "square":
-        answer = square(num1)
-        print(answer)
-
-    elif tokens[0] == "cube":
-        answer = cube(num1)
-        print(answer)
-
-    elif tokens[0] == "power":
-        answer = power(num1, num2)
-        print(answer)
-
-    elif tokens[0] == "mod":
-        answer = mod(num1, num2)
-        print(answer)
+calculator_2()
