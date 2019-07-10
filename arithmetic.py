1ce6138341d33bccae085c6f9782e364a5cd6044
Hackbright Student <no-reply@hackbrightacademy.com>
2019-07-10 12:55:42 -0700

    Multiply and divide completed

--- arithmetic.py
+++ arithmetic.py
@@ -10,15 +10,19 @@ def add(num1, num2):
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
 
-    return (num1-num2)
+    return (num1 - num2)
 
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
 
+    return (num1 * num2)
+
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
+
+    return (num1 / num2)
 
 
 def square(num1):
